e090fa41795e3c9c483385356a6b586509e7145a
joppe <jboeve009@gmail.com>
2026-05-17 17:22:12 +0200

    Youtube vid

--- image-harvester/src/image_harvester/__init__.py
+++ image-harvester/src/image_harvester/__init__.py
@@ -10,26 +10,7 @@ def main1() -> None:
 
 
 def main() -> None:
-    main1()
 
-    # Create a new YOLO model from scratch
-    model = YOLO("yolo26n.yaml")
+    model = YOLO("yolo26n.pt")
+    results = model.track(source="https://youtu.be/YzcawvDGe4Y", show=True)
 
-    # # Load a pretrained YOLO model (recommended for training)
-    # model = YOLO("yolo26n.pt")
-
-    # Train the model using the 'coco8.yaml' dataset for 3 epochs
-    # results = model.train(data="coco8.yaml", epochs=3, device="cpu")
-    results = model.train(data="coco8.yaml", epochs=3)
-
-    # Evaluate the model's performance on the validation set
-    results = model.val()
-
-    # Perform object detection on an image using the model
-    results = model("https://ultralytics.com/images/bus.jpg")
-
-    # Export the model to ONNX format
-    success = model.export(format="onnx")
-
-
-main()
