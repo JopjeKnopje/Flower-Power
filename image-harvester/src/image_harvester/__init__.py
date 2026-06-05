1ad11488970f99804190b10c4e7e87cd560a6c47
joppe <jboeve009@gmail.com>
2026-06-05 13:56:10 +0200

    add tracking example

--- image-harvester/src/image_harvester/__init__.py
+++ image-harvester/src/image_harvester/__init__.py
@@ -1,42 +1,61 @@
-from ultralytics import YOLO  # pyright: ignore[reportPrivateImportUsage]
-import cv2
+from collections import defaultdict
 
+import cv2
+import numpy as np
+
+from ultralytics import YOLO
 
 def main() -> None:
+
     # Load the YOLO26 model
     model = YOLO("yolo26n.pt")
-
+    
     # Open the video file
-    cap = cv2.VideoCapture("test-vids/video1.mp4")
-
+    video_path = "test-vids/top-down.mp4"
+    cap = cv2.VideoCapture(video_path)
+    
+    # Store the track history
+    track_history = defaultdict(lambda: [])
+    
     # Loop through the video frames
     while cap.isOpened():
         # Read a frame from the video
         success, frame = cap.read()
-
+    
         if success:
             # Run YOLO26 tracking on the frame, persisting tracks between frames
             result = model.track(frame, persist=True)[0]
-
-            object_count: int = 0
-
-            # super gnarly
-            for box in result.boxes:
-                if model.names[int(box.cls)] == "person":
-                    object_count += 1
-
-            print(f"people count {object_count}")
-            annotated_frame = result.plot()
-
+    
+            # Get the boxes and track IDs
+            if result.boxes and result.boxes.is_track:
+                boxes = result.boxes.xywh.cpu()
+                track_ids = result.boxes.id.int().cpu().tolist()
+    
+                # Visualize the result on the frame
+                frame = result.plot()
+    
+                # Plot the tracks
+                for box, track_id in zip(boxes, track_ids):
+                    x, y, w, h = box
+                    track = track_history[track_id]
+                    track.append((float(x), float(y)))  # x, y center point
+                    if len(track) > 30:  # retain 30 tracks for 30 frames
+                        track.pop(0)
+    
+                    # Draw the tracking lines
+                    points = np.hstack(track).astype(np.int32).reshape((-1, 1, 2))
+                    cv2.polylines(frame, [points], isClosed=False, color=(230, 230, 230), thickness=10)
+    
             # Display the annotated frame
-            cv2.imshow("YOLO26 Tracking", annotated_frame)
-
+            cv2.imshow("YOLO26 Tracking", frame)
+    
             # Break the loop if 'q' is pressed
             if cv2.waitKey(1) & 0xFF == ord("q"):
                 break
         else:
+            # Break the loop if the end of the video is reached
             break
-
+    
     # Release the video capture object and close the display window
     cap.release()
     cv2.destroyAllWindows()
