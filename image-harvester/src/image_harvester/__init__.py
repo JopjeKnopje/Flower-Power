2bb520d7e1c9cf1bd673fd5bd66a4598e28cd09d
joppe <jboeve009@gmail.com>
2026-06-09 01:13:19 +0200

    Can draw lines on peepz hehe

--- image-harvester/src/image_harvester/__init__.py
+++ image-harvester/src/image_harvester/__init__.py
@@ -1,61 +1,87 @@
 from collections import defaultdict
+import random
+from typing import Self
 
 import cv2
 import numpy as np
 
 from ultralytics import YOLO
 
+from image_harvester_test import find_line
+from shapes import Color, Line, Point
+
+
 def main() -> None:
 
     # Load the YOLO26 model
     model = YOLO("yolo26n.pt")
-    
+
     # Open the video file
     video_path = "test-vids/top-down.mp4"
     cap = cv2.VideoCapture(video_path)
-    
-    # Store the track history
-    track_history = defaultdict(lambda: [])
-    
+
+    frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+    # which is a simple line for now (we should actually use a point for this)
+    flower_location = Line(
+        [200, frame_height - 25], [frame_width - 200, frame_height - 25], 0xFFFF11
+    )
+    flower_location.set_color(Color.RED)
+
     # Loop through the video frames
     while cap.isOpened():
         # Read a frame from the video
         success, frame = cap.read()
-    
+
+        flower_location.draw(frame)
+
         if success:
             # Run YOLO26 tracking on the frame, persisting tracks between frames
             result = model.track(frame, persist=True)[0]
-    
+
             # Get the boxes and track IDs
             if result.boxes and result.boxes.is_track:
                 boxes = result.boxes.xywh.cpu()
                 track_ids = result.boxes.id.int().cpu().tolist()
-    
+
                 # Visualize the result on the frame
                 frame = result.plot()
-    
+
                 # Plot the tracks
                 for box, track_id in zip(boxes, track_ids):
                     x, y, w, h = box
-                    track = track_history[track_id]
-                    track.append((float(x), float(y)))  # x, y center point
-                    if len(track) > 30:  # retain 30 tracks for 30 frames
-                        track.pop(0)
-    
-                    # Draw the tracking lines
-                    points = np.hstack(track).astype(np.int32).reshape((-1, 1, 2))
-                    cv2.polylines(frame, [points], isClosed=False, color=(230, 230, 230), thickness=10)
-    
+
+                    p = Point((int(x), int(y)))
+                    proj = find_line(p, flower_location)
+                    proj.set_color(Color.LIGHT_BLUE)
+                    proj.draw(frame)
+
+                    # track = track_history[track_id]
+                    # track.append((float(x), float(y)))  # x, y center point
+                    # if len(track) > 30:  # retain 30 tracks for 30 frames
+                    #     track.pop(0)
+
+                    # # Draw the tracking lines
+                    # points = np.hstack(track).astype(np.int32).reshape((-1, 1, 2))
+                    # cv2.polylines(
+                    #     frame,
+                    #     [points],
+                    #     isClosed=False,
+                    #     color=(230, 230, 230),
+                    #     thickness=10,
+                    # )
+
             # Display the annotated frame
             cv2.imshow("YOLO26 Tracking", frame)
-    
+
             # Break the loop if 'q' is pressed
             if cv2.waitKey(1) & 0xFF == ord("q"):
                 break
         else:
             # Break the loop if the end of the video is reached
             break
-    
+
     # Release the video capture object and close the display window
     cap.release()
     cv2.destroyAllWindows()
--- image-harvester/src/image_harvester_test/__init__.py
+++ image-harvester/src/image_harvester_test/__init__.py
@@ -2,7 +2,7 @@ from math import sqrt
 import cv2
 import numpy as np
 
-from shapes import Line, Point, Shape
+from shapes import Color, Line, Point, Shape
 
 WINDOW_WIDTH = 800
 WINDOW_HEIGHT = 600
@@ -11,7 +11,7 @@ point_location = (500, 500)
 
 
 def point_to_point_dist(p1: Point, p2: Point) -> float:
-    return sqrt(abs((p1.p1[0] - p2.p1[0])**2 + (p1.p1[1] - p2.p1[1])**2))
+    return sqrt(abs((p1.p1[0] - p2.p1[0]) ** 2 + (p1.p1[1] - p2.p1[1]) ** 2))
 
 
 # TODO : Understand this stuff
@@ -19,27 +19,25 @@ def point_to_point_dist(p1: Point, p2: Point) -> float:
 # https://en.wikipedia.org/wiki/Distance_from_a_point_to_a_line#A_vector_projection_proof
 def find_line(p: Point, line: Line) -> Line:
 
-    line_squared = (line.p1[0] - line.p2[0])**2 + (line.p1[1] - line.p2[1])**2
-    if (line_squared == 0.0):
+    line_squared = (line.p1[0] - line.p2[0]) ** 2 + (line.p1[1] - line.p2[1]) ** 2
+    if line_squared == 0.0:
         # TODO: See when this gets triggered
         return Line(p.p1, line.p1)
-    dot_product = (
-        (p.p1[0] - line.p1[0]) * (line.p2[0] - line.p1[0]) +
-        (p.p1[1] - line.p1[1]) * (line.p2[1] - line.p1[1])
-        )
+    dot_product = (p.p1[0] - line.p1[0]) * (line.p2[0] - line.p1[0]) + (
+        p.p1[1] - line.p1[1]
+    ) * (line.p2[1] - line.p1[1])
     t = max(0, min(1, dot_product / line_squared))
     vec = (
         int(line.p1[0] + t * (line.p2[0] - line.p1[0])),
-        int(line.p1[1] + t * (line.p2[1] - line.p1[1]))
+        int(line.p1[1] + t * (line.p2[1] - line.p1[1])),
     )
 
     proj = Point(vec)
-    dist = point_to_point_dist(p, proj)
+    # dist = point_to_point_dist(p, proj)
 
-    print(f"t: {proj}")
-    print(f" p: {p.p1} | proj {proj.p1} | distance: {dist}")
+    # print(f"t: {proj}")
+    # print(f" p: {p.p1} | proj {proj.p1} | distance: {dist}")
     return Line(p.p1, proj.p1)
-
 
 
 def main() -> None:
@@ -48,28 +46,24 @@ def main() -> None:
         Point([100, 100]),
         Point([45, 120]),
         Line([50, 100], [100, 50]),
-        Line([58, 150], [200, 50])
+        Line([58, 150], [200, 50], Color.LIGHT_BLUE),
     ]
-
 
     # TODO: Fix polymorhpism
     l = find_line(points[0], points[2])
-    l.set_color(0xff1111)
+    l.set_color(Color.BABY_BLUE)
     l.draw(frame)
 
     l = find_line(points[1], points[2])
-    l.set_color(0xff1111)
+    l.set_color(Color.BABY_BLUE)
     l.draw(frame)
     # Line(points[0], line_intersect)
     # print(f"distance {distance}")
 
-
     for p in points:
         p.draw(frame)
 
-
     cv2.imshow("test", frame)
     _ = cv2.waitKey(0)
-
 
     cv2.destroyAllWindows()
