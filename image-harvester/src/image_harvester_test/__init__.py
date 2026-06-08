d8ea797685109dd241f105ab0ca2975a7bb9b9e3
joppe <jboeve009@gmail.com>
2026-06-09 00:11:17 +0200

    Got the line drawing working finally

--- image-harvester/src/image_harvester_test/__init__.py
+++ image-harvester/src/image_harvester_test/__init__.py
@@ -24,11 +24,16 @@ class Color(EnumDict):
 @dataclass
 class Shape(ABC):
     p1: Vec2i
-    color: Scalar = Color.WHITE
+    _color: Scalar = Color.WHITE
 
     @abstractmethod
     def draw(self, frame: MatLike) -> None:
         pass
+
+    def set_color(self, color: Scalar) -> None:
+        self._color = color
+
+
 
 class Point(Shape):
     @override
@@ -36,15 +41,17 @@ class Point(Shape):
         x = self.p1[0]
         y = self.p1[1]
         thickness = 5
-        _ = cv2.rectangle(frame, (x, y), (x + thickness, y + thickness), self.color, -1)
+        _ = cv2.rectangle(frame, (x, y), (x + thickness, y + thickness), self._color, -1)
 
 
 class Line(Shape):
     p2: Vec2i
 
-    def __init__(self, position: Vec2i, end: Vec2i, color: Scalar=Shape.color) -> None:
+    def __init__(self, position: Vec2i, end: Vec2i, color: Scalar=Shape._color) -> None:
         Shape.__init__(self, position, color)
         self.p2 = end
+
+   
 
 
     @override
@@ -54,54 +61,59 @@ class Line(Shape):
         end_x = self.p2[0]
         end_y = self.p2[1]
 
-        _ = cv2.line(frame, (start_x, start_y), (end_x, end_y), self.color, 1)
+        _ = cv2.line(frame, (start_x, start_y), (end_x, end_y), self._color, 1)
 
 
+def point_to_point_dist(p1: Point, p2: Point) -> float:
+    return sqrt(abs((p1.p1[0] - p2.p1[0])**2 + (p1.p1[1] - p2.p1[1])**2))
 
-# https://en.wikipedia.org/wiki/Distance_from_a_point_to_a_line#Line_defined_by_two_points
-def find_distance(p: Point, line: Line) -> float:
-    numerator  = abs(
-        (line.p1[1] - line.p2[1]) * p.p1[0]
-        -
-        (line.p1[0] - line.p2[0]) * p.p1[1]
-        + 
-        (line.p2[0] * line.p1[1]) -  (line.p2[1] * line.p1[0])
-    )
-    denominator = sqrt((line.p1[0] - line.p2[0])**2 - (line.p1[1] - line.p2[1])**2)
 
-    return numerator / denominator
-
+# TODO : Understand this stuff
+# https://stackoverflow.com/a/1501725/7363348
+# https://en.wikipedia.org/wiki/Distance_from_a_point_to_a_line#A_vector_projection_proof
 def find_line(p: Point, line: Line) -> Line:
-    # 1. calculate slope of `line` called `m`
-    m  = (line.p1[1] - line.p2[1]) / (line.p1[0] - line.p2[0])
-    # 2. get angle of slope and rotate 90 degrees it its perpendicular to `line`
-    # TODO: Remove this step and use `m` directly
-    l_angle = atan(m) + radians(90)
-    # 3. construct new equation for us to draw a new line with intersecting `line`
-    m_new = degrees(tan(l_angle))
 
+    line_squared = (line.p1[0] - line.p2[0])**2 + (line.p1[1] - line.p2[1])**2
+    if (line_squared == 0.0):
+        # TODO: See when this gets triggered
+        return Line(p.p1, line.p1)
+    dot_product = (
+        (p.p1[0] - line.p1[0]) * (line.p2[0] - line.p1[0]) +
+        (p.p1[1] - line.p1[1]) * (line.p2[1] - line.p1[1])
+        )
+    t = max(0, min(1, dot_product / line_squared))
+    vec = (
+        int(line.p1[0] + t * (line.p2[0] - line.p1[0])),
+        int(line.p1[1] + t * (line.p2[1] - line.p1[1]))
+    )
+
+    proj = Point(vec)
+    dist = point_to_point_dist(p, proj)
+
+    print(f"t: {proj}")
+    print(f" p: {p.p1} | proj {proj.p1} | distance: {dist}")
+    return Line(p.p1, proj.p1)
 
 
 
 def main() -> None:
     frame = np.zeros((WINDOW_HEIGHT, WINDOW_WIDTH, 3), np.uint8)
     points: list[Shape] = [
-        Point([*point_location]),
-        Point([25, 25]),
-        Line([52, 81], [138, 100])
+        Point([100, 100]),
+        Line([5, 50], [50, 5])
     ]
-
 
 
     for p in points:
         p.draw(frame)
 
 
-    distance = find_distance(points[0], points[2])
-
-    find_line(points[0], points[2])
+    # TODO: Fix polymorhpism
+    l = find_line(points[0], points[1])
+    l.set_color(0xff1111)
+    l.draw(frame)
     # Line(points[0], line_intersect)
-    print(f"distance {distance}")
+    # print(f"distance {distance}")
 
 
 
