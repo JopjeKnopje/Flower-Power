89b915d7e0e2fc4e05475fd2b14df8d09e5473a8
joppe <jboeve009@gmail.com>
2026-06-09 00:30:00 +0200

    Add shapes as module

--- image-harvester/src/image_harvester_test/__init__.py
+++ image-harvester/src/image_harvester_test/__init__.py
@@ -1,67 +1,13 @@
-from abc import ABC, abstractmethod
-from dataclasses import dataclass
-from enum import EnumDict
-from re import S
-from typing import override
-from math import sqrt, atan, tan, radians, degrees
-
+from math import sqrt
 import cv2
 import numpy as np
-from cv2.typing import (
-    MatLike,
-    Scalar,
-    Vec2i,
-)
+
+from shapes import Line, Point, Shape
 
 WINDOW_WIDTH = 800
 WINDOW_HEIGHT = 600
 
 point_location = (500, 500)
-
-class Color(EnumDict):
-    WHITE: Scalar = (255,255,255)
-
-@dataclass
-class Shape(ABC):
-    p1: Vec2i
-    _color: Scalar = Color.WHITE
-
-    @abstractmethod
-    def draw(self, frame: MatLike) -> None:
-        pass
-
-    def set_color(self, color: Scalar) -> None:
-        self._color = color
-
-
-
-class Point(Shape):
-    @override
-    def draw(self, frame: MatLike) -> None:
-        x = self.p1[0]
-        y = self.p1[1]
-        thickness = 5
-        _ = cv2.rectangle(frame, (x, y), (x + thickness, y + thickness), self._color, -1)
-
-
-class Line(Shape):
-    p2: Vec2i
-
-    def __init__(self, position: Vec2i, end: Vec2i, color: Scalar=Shape._color) -> None:
-        Shape.__init__(self, position, color)
-        self.p2 = end
-
-   
-
-
-    @override
-    def draw(self, frame: MatLike) -> None:
-        start_x = self.p1[0]
-        start_y = self.p1[1]
-        end_x = self.p2[0]
-        end_y = self.p2[1]
-
-        _ = cv2.line(frame, (start_x, start_y), (end_x, end_y), self._color, 1)
 
 
 def point_to_point_dist(p1: Point, p2: Point) -> float:
