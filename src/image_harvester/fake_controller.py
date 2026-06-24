423d8e62f86d83197ad4515d7f1fce0a85531820
joppe <jboeve009@gmail.com>
2026-06-24 13:58:23 +0200

    WIP: Add very basic webserver

--- src/image_harvester/fake_controller.py
+++ src/image_harvester/fake_controller.py
@@ -1,20 +1,41 @@
+from dataclasses import dataclass
 import http.server
 import socketserver
 from typing import override
 
-PORT = 8000
+PORT = 8003
+
+
+
+class FlowerCylinder:
+    _position_mm: int
+
+    def __init__(self) -> None:
+        self._position_mm = 0
 
 
 class CustomHandler(http.server.SimpleHTTPRequestHandler):
+
+
     @override
     def do_GET(self) -> None:
 
         print(self.path)
 
-        super().do_GET()
+        self.send_response(301)
+        self.send_header('content-type', 'text/html')
+        self.end_headers()
+        _ = self.wfile.write("reply text".encode())
 
 
+
+# TODO: register routes
 def fake_controller() -> None:
-    with socketserver.TCPServer(("", PORT), CustomHandler) as httpd:
-        print(f"Serving at port {PORT}, ready to handle POST requests")
-        httpd.serve_forever()
+
+    httpd = socketserver.TCPServer(("", PORT), CustomHandler)
+    httpd.allow_reuse_port = True
+    httpd.allow_reuse_address = True
+    print(f"webserver running at port {PORT}")
+    httpd.serve_forever()
+
+
